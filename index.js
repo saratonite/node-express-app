a800800f5c55b96884ffad237c7bbd50c798ac02
Sarath <sarath.hacks@gmail.com>
2016-05-12 15:12:28 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+
+app.get('/',function(req,res){
+
+	res.send('Hello World');
+
+});
+
+app.listen(8585,function(){
+	console.log('Listening on port : 8585');
+});
