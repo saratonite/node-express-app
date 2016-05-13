3582a58b05026425af32dba3ad2611e7bd5e33ce
Sarath <sarath.hacks@gmail.com>
2016-05-13 08:11:24 +0530

    Added express-handlebars

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var express = require('express');
 
+var port = process.env.port || 8585;
+
 var app = express();
 
+var exphbs  = require('express-handlebars');
 
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 app.get('/',function(req,res){
 
-	res.send('Hello World');
+	//res.sendfile('./public/index.html');
+	res.render('home');
 
 });
 
-app.listen(8585,function(){
-	console.log('Listening on port : 8585');
+app.listen(port,function(){
+	console.log('Listening on port :'+port);
 });
