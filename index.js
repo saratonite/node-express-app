2c677b35bdc05a62f7a278eaa7f0021d47ac9901
Sarath <sarath.hacks@gmail.com>
2016-05-13 09:56:14 +0530

    Mysql database connection , users page

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ var app = express();
 
 var exphbs  = require('express-handlebars');
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({ // Mysql Connection
+    host : 'localhost',
+    user : 'root',
+    password : 'root',
+    database : 'test',
+});
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -17,6 +26,20 @@ app.get('/',function(req,res){
 	//res.sendfile('./public/index.html');
 	res.render('home');
 
+});
+
+app.get('/data',function(req,res){
+
+	connection.query('select * from users',function(err,rows,fields){
+
+		console.log(err);
+		console.log(fields);
+
+		//res.json(rows);
+
+		res.render('users',{"users":rows});
+
+	});
 });
 
 app.listen(port,function(){
